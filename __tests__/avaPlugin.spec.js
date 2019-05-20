c3fe3e6a67672d6e7b7c143f34f14f595ff7a519
dnlup <dwon.dnl@gmail.com>
2019-05-20 20:40:41 +0200

    test: fix yarn install errors
    Force user preference to use npm on a CI environment to avoid yarn install errors.
    See https://github.com/yarnpkg/yarn/issues/2629.

--- __tests__/avaPlugin.spec.js
+++ __tests__/avaPlugin.spec.js
@@ -1,12 +1,15 @@
 const { join, resolve } = require('path')
 const { promisify } = require('util')
-const mkdir = promisify(require('fs').mkdir)
+const { homedir } = require('os')
+const { mkdir, writeFile } = require('fs')
+const mkd = promisify(mkdir)
+const writeF = promisify(writeFile)
 const rimraf = promisify(require('rimraf'))
 const test = require('ava')
 const id = require('nanoid/generate')
 const create = require('@vue/cli-test-utils/createTestProject')
 const { version: VERSION } = require('../package.json')
-
+const isCI = require('is-ci')
 const ROOT = resolve(__dirname, '../')
 const DATA_DIR = join(__dirname, '.data')
 
@@ -25,7 +28,21 @@ async function plugin (t, input = {}, expected) {
 
 test.before(async t => {
   try {
-    await mkdir(DATA_DIR)
+    await mkd(DATA_DIR)
+    // If in a CI env manually set user preference to use npm
+    // in order to avoid yarn errors when installing
+    // see https://github.com/yarnpkg/yarn/issues/2629
+    if (isCI) {
+      const data = {
+        useTaobaoRegistry: false,
+        latestVersion: '3.7.0',
+        lastChecked: 1556454509806,
+        packageManager: 'npm',
+        presets: {}
+      }
+      const file = join(homedir(), '.vuerc')
+      await writeF(file, JSON.stringify(data), 'utf8')
+    }
   } catch (error) {}
 })
 
