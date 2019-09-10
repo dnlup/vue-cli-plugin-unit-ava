f4dd00e0a5d38e3f92ad7f23b037e89836fad299
dnlup <dwon.dnl@gmail.com>
2019-09-10 21:13:29 +0200

    build(scripts/publish): use npm otp code
    Use OTP code instead of a token.

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,39 +1,33 @@
 const inquirer = require('inquirer')
 const execa = require('execa')
 
-inquirer.prompt([
-  {
-    type: 'password',
-    name: 'NPM_AUTH_TOKEN',
-    message: 'npm token',
-    filter (input) {
-      return input.trim()
+;(async () => {
+  const { NPM_OTP, CONVENTIONAL_GITHUB_RELEASER_TOKEN } = await inquirer.prompt([
+    {
+      type: 'password',
+      name: 'NPM_OTP',
+      message: 'npm OTP code',
+      filter (input) {
+        return input.trim()
+      },
+      validate (input, answers) {
+        return input && input.length > 0 ? true : 'Cannot be emtpy'
+      }
     },
-    validate (input, answers) {
-      return input && input.length > 0 ? true : 'Cannot be emtpy'
+    {
+      type: 'password',
+      name: 'CONVENTIONAL_GITHUB_RELEASER_TOKEN',
+      message: 'GitHub Token (for GitHub Releases)',
+      filter (input) {
+        return input.trim()
+      },
+      validate (input, answers) {
+        return input && input.length > 0 ? true : 'Cannot be emtpy'
+      }
     }
-  },
-  {
-    type: 'password',
-    name: 'CONVENTIONAL_GITHUB_RELEASER_TOKEN',
-    message: 'GitHub Token (for GitHub Releases)',
-    filter (input) {
-      return input.trim()
-    },
-    validate (input, answers) {
-      return input && input.length > 0 ? true : 'Cannot be emtpy'
-    }
-  }
-]).then(async answers => {
-  const { NPM_AUTH_TOKEN, CONVENTIONAL_GITHUB_RELEASER_TOKEN } = answers
-  await execa('npm', ['publish'], {
-    env: {
-      NPM_AUTH_TOKEN
-    }
-  })
-  await execa('conventional-github-releaser', ['-p', 'angular'], {
-    env: {
-      CONVENTIONAL_GITHUB_RELEASER_TOKEN
-    }
-  })
-})
+  ])
+  await execa('npm', ['publish', '--otp', NPM_OTP])
+  await execa('conventional-github-releaser', [
+    '-p', 'angular', '-t', CONVENTIONAL_GITHUB_RELEASER_TOKEN
+  ])
+})()
