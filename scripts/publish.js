d93c5604b5131582a71a5e46183ed01b6817e46b
dnlup <dwon.dnl@gmail.com>
2019-09-10 21:24:43 +0200

    build(scripts/publish): handle errors and prompt for otp last

--- scripts/publish.js
+++ scripts/publish.js
@@ -2,32 +2,41 @@ const inquirer = require('inquirer')
 const execa = require('execa')
 
 ;(async () => {
-  const { NPM_OTP, CONVENTIONAL_GITHUB_RELEASER_TOKEN } = await inquirer.prompt([
-    {
-      type: 'password',
-      name: 'NPM_OTP',
-      message: 'npm OTP code',
-      filter (input) {
-        return input.trim()
+  let code = 0
+  try {
+    const { NPM_OTP, CONVENTIONAL_GITHUB_RELEASER_TOKEN } = await inquirer.prompt([
+      {
+        type: 'password',
+        name: 'CONVENTIONAL_GITHUB_RELEASER_TOKEN',
+        message: 'GitHub Token (for GitHub Releases)',
+        filter (input) {
+          return input.trim()
+        },
+        validate (input, answers) {
+          return input && input.length > 0 ? true : 'Cannot be emtpy'
+        }
       },
-      validate (input, answers) {
-        return input && input.length > 0 ? true : 'Cannot be emtpy'
+      {
+        type: 'password',
+        name: 'NPM_OTP',
+        message: 'npm OTP code',
+        filter (input) {
+          return input.trim()
+        },
+        validate (input, answers) {
+          return input && input.length > 0 ? true : 'Cannot be emtpy'
+        }
       }
-    },
-    {
-      type: 'password',
-      name: 'CONVENTIONAL_GITHUB_RELEASER_TOKEN',
-      message: 'GitHub Token (for GitHub Releases)',
-      filter (input) {
-        return input.trim()
-      },
-      validate (input, answers) {
-        return input && input.length > 0 ? true : 'Cannot be emtpy'
-      }
-    }
-  ])
-  await execa('npm', ['publish', '--otp', NPM_OTP])
-  await execa('conventional-github-releaser', [
-    '-p', 'angular', '-t', CONVENTIONAL_GITHUB_RELEASER_TOKEN
-  ])
+    ])
+
+    await execa('npm', ['publish', '--otp', NPM_OTP])
+    await execa('conventional-github-releaser', [
+      '-p', 'angular', '-t', CONVENTIONAL_GITHUB_RELEASER_TOKEN
+    ])
+  } catch (error) {
+    code = 1
+    console.error(error)
+  } finally {
+    process.exit(code)
+  }
 })()
